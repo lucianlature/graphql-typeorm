90037d87f62b1ef5c5bd4aa620812590808e5ad9
Lucian Lature <lucian.lature@gmail.com>
2016-12-18 19:16:57 +0000

    Added support for babel, eslint, flow and jest.

---
+++ src/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  resolver: require('./resolver'),
+  // defaultListArgs: require('./defaultListArgs'),
+  // defaultArgs: require('./defaultArgs'),
+  // typeMapper: require('./typeMapper'),
+  // attributeFields: require('./attributeFields'),
+  simplifyAST: require('./simplifyAST'),
+  // relay: require('./relay')
+};
